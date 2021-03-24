c51b0b28037a92fb76b4f2cbdebf547d6c54ef25
Hollis Lehv <hlehv@aol.com>
2021-03-24 14:30:28 -0400

    Add board class to TTT

--- TicTacToe/tictactoe.cpp
+++ TicTacToe/tictactoe.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
-#include <tictactoe.h>
+#include "tictactoe.h"
+
+using namespace std; 
+
+enum Player { human, computer };
+
+class Board {
+    public:
+        Board(Player player1, Player player2) : p1(player1), p2(player2) {}
+    private:
+        Player p1;
+        Player p2;
+        Player currentplayer;
+};
 
 int main (int argc, char **argv)
 {
     return 1;
-
 }
