c85aa3033a3859af0216befd323c864480ca9851
Hollis Lehv <hlehv@aol.com>
2021-03-24 17:35:38 -0400

    Add board and game, board printing

--- TicTacToe/tictactoe.cpp
+++ TicTacToe/tictactoe.cpp
@@ -1,20 +1,57 @@
 #include <iostream>
 #include "tictactoe.h"
+#include <vector>
 
 using namespace std; 
 
-enum Player { human, computer };
+Board::Board(size_t size)
+{
+    board.resize(size, vector<char>(size));
+    for (size_t i = 0; i < size; i++)
+    {
+        for (size_t j = 0; j < size; j++)
+        {
+            board[i][j] = ' ';
+        }
+        
+    }     
+}
 
-class Board {
-    public:
-        Board(Player player1, Player player2) : p1(player1), p2(player2) {}
-    private:
-        Player p1;
-        Player p2;
-        Player currentplayer;
-};
+void Board::printBoard()
+{
+    
+    for (auto row = board.begin(); row != board.end(); ++row)
+    {
+        string baseline;
+        for (auto col = row->begin(); col != row->end(); ++col)
+        {
+            baseline += "----";
+            cout << *col;
+            if (col+1 != row->end())
+            {
+                cout << "  |";
+            }
+            
+        }
+        cout << endl;
+        if (row+1 != board.end())
+        {
+            cout << baseline << endl;
+        }
+    }
+}
+
+Game::Game(Player player1, Player player2, int size = 3) 
+    : board(size), 
+    p1(player1),
+    p2(player2),
+    currentplayer(player1)
+{
+
+}
 
 int main (int argc, char **argv)
 {
-    return 1;
+    Board board(4);
+    board.printBoard();
 }
--- TicTacToe/tictactoe.h
+++ TicTacToe/tictactoe.h
@@ -1,5 +1,29 @@
 #ifndef TICTACTOE_H
 #define TICTACTOE_H
 
+#include <vector>
+
+using namespace std; 
+enum Player { human, computer };
+
+class Board {
+    public:
+        bool isTerminal();
+        Board(size_t size);
+        void printBoard();
+    private:
+        vector<vector<char>> board;
+};
+
+class Game {
+    public:
+        Game(Player player1, Player player2, int size);
+    private:
+        Board board;
+        Player p1;
+        Player p2;
+        Player currentplayer;
+        
+};
 
 #endif
