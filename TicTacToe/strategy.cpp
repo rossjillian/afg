083758ed22cdbb1b86ad2306b5c6862d8b72436b
Jillian Ross <jillian.ross@columbia.edu>
2021-03-31 22:36:19 -0400

    Update strategy.cpp
    Removed left-over print statements from testing

--- TicTacToe/strategy.cpp
+++ TicTacToe/strategy.cpp
@@ -12,13 +12,7 @@ int random_tile(const TicTacToe& state) {
     vector<int> possibleMoves = state.getAvailableMoves();
     auto rng = std::default_random_engine {};
 	
-    for (std::vector<int>::const_iterator i = possibleMoves.begin(); i != possibleMoves.end(); ++i)
-        cout << *i << endl;
-	
     std::shuffle(possibleMoves.begin(), possibleMoves.end(), rng);
-	
-    for (std::vector<int>::const_iterator i = possibleMoves.begin(); i != possibleMoves.end(); ++i)
-        cout << *i << endl;
 	
     return possibleMoves[0];
 }
