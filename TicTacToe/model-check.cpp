5c42a31b7a6cd8003b84c398df2c1468ced08c15
Hans Montero <hjm2133@columbia.edu>
2021-04-01 20:58:28 -0400

    Add comments to model-check.cpp

--- TicTacToe/model-check.cpp
+++ TicTacToe/model-check.cpp
@@ -10,18 +10,41 @@ int main(int argc, char **argv) {
     TicTacToe ttt(config);
 
     vector<function<bool(const TicTacToe&)>> predicates;
-    predicates.push_back([](const TicTacToe& st) { return st.b.board[0][0] == 'x' && st.b.board[0][1] == 'x' && st.b.board[1][1] == 'x'; });
-    predicates.push_back([](const TicTacToe& st) { return st.b.isWinner() && (st.getWinner() == 1); });
-    /* gcauto res = Model::bfsFind(ttt, predicates[1], 10); */
-    /* for (const auto& ttt : res.matches) { */
-    /*     string repr; */
-    /*     for (auto& row : ttt.b.board) { */
-    /*         for (auto& c : row) { */
-    /*             repr += c; */
-    /*         } */
-    /*     } */
-    /*     cout << repr << endl; */
-    /* } */
 
-    cout << Model::pathExists(ttt, predicates, 3) << endl;
+    /* Predicate #1: Find TTT states that look like this:
+     *   x | x |
+     *  -----------
+     *     | x |
+     *  -----------
+     *     |   |
+     */
+    predicates.push_back(
+        [](const TicTacToe& st) {
+            return (st.b.board[0][0] == 'x'
+                    && st.b.board[0][1] == 'x'
+                    && st.b.board[1][1] == 'x');
+        }
+    );
+
+    /* Predicate #2: Find a TTT state where Player1 ('x') wins */
+    predicates.push_back(
+        [](const TicTacToe& st) {
+             return st.b.isWinner() && (st.getWinner() == 1);
+        }
+    );
+
+    /* Print all boards that match predicate #1 */
+    auto res = Model::bfsFind(ttt, predicates[1], 10);
+    for (const auto& ttt : res.matches) {
+        string repr;
+        for (auto& row : ttt.b.board) {
+            for (auto& c : row) {
+                repr += c;
+            }
+        }
+        cout << repr << endl;
+    }
+
+    /* Check if it's possible to satisfy predicate #1 and #2 in 7 moves */
+    cout << Model::pathExists(ttt, predicates, 7) << endl;
 }
