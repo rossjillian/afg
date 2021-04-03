f160d5a83178f8ec46b031994d09e0fd68ae52d3
Jillian Ross <jillian.ross@columbia.edu>
2021-04-03 15:20:46 -0400

    WIP: abstracting AI

--- TicTacToe/strategy.hpp
+++ TicTacToe/strategy.hpp
@@ -6,7 +6,5 @@
 int random_tile(const TicTacToe& state);
 int io_tile(const TicTacToe& state);
 int minimax_tile(const TicTacToe& state, int player = 0);
-int max(TicTacToe& state, int& best_move);
-int min(TicTacToe& state, int& best_move);
 
 #endif
