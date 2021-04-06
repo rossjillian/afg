863a991a1837c60b35574877fa75c3b5a172717c
Jillian Ross <jillian.ross@columbia.edu>
2021-04-06 09:36:17 -0400

    Alpha beta pruning

--- include/ai.hpp
+++ include/ai.hpp
@@ -7,42 +7,48 @@
 
 namespace AI {
     template<Playable GameType, IntelligentPlayer<GameType> P>
-    int minimizer(GameType& state, int& bestMove, P player);
+    int minimizer(GameType& state, int& bestMove, P player, int alpha, int beta);
 	
     template<Playable GameType, IntelligentPlayer<GameType> P>
-    int maximizer(GameType& state, int& bestMove, P player) {
+    int maximizer(GameType& state, int& bestMove, P player, int alpha, int beta) {
 	if (state.isTerminal())
             return player.heuristic(state);
 
         int val = std::numeric_limits<int>::min();
         vector<typename GameType::move_t> possibleMoves = state.getAvailableMoves();
-        for (typename GameType::move_t move : possibleMoves) {
+        for (auto move : possibleMoves) {
             GameType stateCopy = state;
             stateCopy.makeMove(move);
-            int newVal = minimizer(stateCopy, bestMove, player);
+            int newVal = minimizer(stateCopy, bestMove, player, alpha, beta);
             if (newVal > val) {
                 val = newVal;
                 bestMove = move;
             }
+            alpha = std::max(alpha, val);
+            if (alpha >= beta)
+                break;
         }
         return val;
     }
 
     template<Playable GameType, IntelligentPlayer<GameType> P>
-    int minimizer(GameType& state, int& bestMove, P player) {
+    int minimizer(GameType& state, int& bestMove, P player, int alpha, int beta) {
         if (state.isTerminal())
             return player.heuristic(state);
 
         int val = std::numeric_limits<int>::max();
         vector<typename GameType::move_t> possibleMoves = state.getAvailableMoves();
-        for (typename GameType::move_t move : possibleMoves) {
+        for (auto move : possibleMoves) {
             GameType stateCopy = state;
             stateCopy.makeMove(move);
-            int newVal = maximizer(stateCopy, bestMove, player);
+            int newVal = maximizer(stateCopy, bestMove, player, alpha, beta);
             if (newVal < val) {
                 val = newVal;
                 bestMove = move;
             }
+            beta = std::min(beta, val);
+            if (beta <= alpha)
+                break;
         }
         return val;
     }
@@ -50,12 +56,14 @@ namespace AI {
     template<Playable GameType, IntelligentPlayer<GameType> P>
     GameType::move_t minimax(const GameType& state, P player) {
         int bestMove;
+        int alpha = std::numeric_limits<int>::min(); 
+        int beta = std::numeric_limits<int>::max();
         GameType stateCopy = state;
         if (player.getParity() == 1) {
-            maximizer(stateCopy, bestMove, player);
+            maximizer(stateCopy, bestMove, player, alpha, beta);
         }
         else {
-            minimizer(stateCopy, bestMove, player);
+            minimizer(stateCopy, bestMove, player, alpha, beta);
         }
         return bestMove;
     }
