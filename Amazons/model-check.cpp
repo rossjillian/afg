59d01893f20f5e5321c40d831b0ebb9907387cab
Hans Montero <hjm2133@columbia.edu>
2021-04-08 02:37:01 -0400

    Add one model-check for Amazon

---
+++ Amazons/model-check.cpp
@@ -0,0 +1,19 @@
+#include <functional>
+#include "amazons.hpp"
+#include "model.hpp"
+
+using namespace afg::model;
+
+int main() {
+    Amazons azns;
+
+    auto st_93_83_93 = [](const Amazons& a) {
+        return (a.b.board[8][3] == 'w'
+                && a.b.board[9][3] == 'x');
+    };
+
+    auto res = bfsFind(azns, st_93_83_93, 1);
+    cout << res.success << endl;
+    cout << res.explored << endl;
+    cout << res.matches.size() << endl;
+}
