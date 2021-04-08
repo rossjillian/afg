ebddbc7b0c408ab5693cd7887cd7911716ce1a9c
Hans Montero <hjm2133@columbia.edu>
2021-04-08 22:17:15 +0000

    Implement amazons model check

--- Amazons/model-check.cpp
+++ Amazons/model-check.cpp
@@ -2,10 +2,12 @@
 #include <ranges>
 #include <array>
 #include <algorithm>
+#include <chrono>
 #include "amazons.hpp"
 #include "model.hpp"
 
 using namespace std;
+using namespace chrono;
 using namespace afg::model;
 
 const int ROWS = 10;
@@ -36,20 +38,47 @@ int main() {
     vector<function<bool(const Amazons&)>> predicates;
     predicates.push_back([](const Amazons& as) {
             /* return whiteSurroundedByArrows(as) == 1; */
-            return as.b.board[0][5] == 'x';
+            return as.b.board[4][5] == 'x';
     });
 
     predicates.push_back([](const Amazons& as) {
             /* return whiteSurroundedByArrows(as) == 1; */
-            return as.b.board[1][5] == 'x';
+            return as.b.board[5][5] == 'x';
     });
 
     predicates.push_back([](const Amazons& as) {
-            /* return whiteSurroundedByArrows(as) == 1; */
-            return as.b.board[2][5] == 'x';
+            for (int i = ROWS * 4; i < ROWS * 6; i++) {
+	        char t = as.getTile(i);
+                if (t == 'w' || t == 'b') return true;
+	    }
+
+            return false;
     });
 
-    cout << pathExists(azns, predicates, 3) << endl;
+    auto t0 = high_resolution_clock::now();
+    cout << "Found?: " << pathExists(azns, predicates, 2) << endl;
+    auto t1 = high_resolution_clock::now();
+    cout << "Staggered: " <<  duration_cast<milliseconds>(t1 - t0).count() << "ms" << endl;
+
+
+    vector<function<bool(const Amazons&)>> bigJump;
+    bigJump.push_back([](const Amazons& as) {
+            /* return whiteSurroundedByArrows(as) == 1; */
+            if (as.b.board[4][5] != 'x' || as.b.board[5][5] != 'x')
+	        return false;
+
+           for (int i = ROWS * 4; i < ROWS * 6; i++) {
+	        char t = as.getTile(i);
+                if (t == 'w' || t == 'b') return true;
+	    }
+
+            return false;
+    });
+
+    t0 = high_resolution_clock::now();
+    cout << "Found?: " << pathExists(azns, bigJump, 2) << endl;
+    t1 = high_resolution_clock::now();
+    cout << "Big Jump: " <<  duration_cast<milliseconds>(t1 - t0).count() << "ms" << endl;
 
     return 0;
 }
